478e589e46392cac176983ca9f1f7ea4b7dcb56c
SiulMJ <luis.esgrima02@gmail.com>
2024-10-07 12:58:13 -0600

    faltan funciones y diseño

--- main.py
+++ main.py
@@ -6,40 +6,57 @@ cur = con.cursor()
 id_map = {}
 
 def insertar():
-    usuario = usu.get()
+    pagina = pag.get()
     contraseña = contra.get()
     descripcion = desc.get()
-    cur.execute("Insert INTO contraseñas (usuario,contraseña,descripcion) values (?,?,?)",( usuario, contraseña, descripcion ))
+    cur.execute("Insert INTO contraseñas (pagina,contraseña,descripcion) values (?,?,?)",( pagina, contraseña, descripcion ))
     con.commit()
     actulist()
 
 def actulist():
     lista.delete(0, tk.END)
     id_map.clear()
-    cur.execute("SELECT id_contra, usuario, contraseña, descripcion FROM contraseñas")
+    cur.execute("SELECT id_contraseñas, pagina, contraseña, descripcion FROM contraseñas")
     con.commit()
     for i, row in enumerate(cur.fetchall()):
         lista.insert(tk.END, f"Usuario: {row[1]}, Contraseña: {row[2]}, Descripción: {row[3]}")
-        id_map[i] = row[0]  # Mapea el índice de la lista al id de la base de datos
+        id_map[i] = row[0]
+
+def registrar(ev):
+    registro = tk.Toplevel()
+    registro.geometry("400x200")
+    registro.title("registrar")
+    reg = ttk.Frame(registro, padding="20")
+    reg.pack(expand=True)
+    tk.Label(reg, text='ingrese su nombre:').grid(row=0, column=0, pady=10, sticky="w")
+    nombre = ttk.Entry(reg, width=30)
+    nombre.grid(row=0, column=1, pady=10)
+
+    tk.Label(reg, text='ingrese su contraseña: ').grid(row=1, column=0, pady=10, sticky="w")
+    contraseña = ttk.Entry(reg, width=30, show="*")
+    contraseña.grid(row=1, column=1, pady=10)
+
+    ttk.Button(reg, text="registrar", command="#").grid(row=2, column=0, columnspan=2, pady=20)
+
 
 def borrar():
     select = lista.curselection()
     if select:
         index = select[0]
         item_id = id_map[index]
-        cur.execute("DELETE FROM contraseñas WHERE id_contra=?", (item_id,))
+        cur.execute("DELETE FROM contraseñas WHERE id_contraseñas=?", (item_id,))
         con.commit()
         actulist()
 
 def modificar():
-    usuario = usu.get()
+    pagina = pagina.get()
     contraseña = contra.get()
     descripcion = desc.get()
     select = lista.curselection()
     if select:
         page = select[0]
         id = id_map[page]
-        con.execute("UPDATE contraseñas SET usuario=?, contraseña=?, descripcion=? where id_contra =?", (usuario, contraseña, descripcion, id))
+        con.execute("UPDATE contraseñas SET pagina=?, contraseña=?, descripcion=? where id_contraseñas =?", (pagina, contraseña, descripcion, id))
     actulist()
 
 def cerrar():
@@ -66,7 +83,15 @@ def inicio():
     tk.Label(login, text='ingrese su contraseña: ').grid(row=1, column=0, pady=10, sticky="w")
     contraseña = ttk.Entry(login, width=30, show="*")
     contraseña.grid(row=1, column=1, pady=10)
-    ttk.Button(login, text="comprobar", command=igual).grid(row=2, column=1, pady=20)
+
+    ttk.Button(login, text="comprobar", command=igual).grid(row=2, column=0, columnspan=2, pady=20)
+    
+    etiqueta = ttk.Label(login, text='¿Notienes usuario?')
+    etiqueta.grid(row=3, column=0, columnspan=2, pady=20)
+    etiqueta.bind('<Button-1>', registrar)
+
+
+    
     ventana2.mainloop()
 
 #ventana de la aplicacion
@@ -81,10 +106,10 @@ texto = ttk.Frame(ventana)
 texto.pack(side='left', fill='both', padx=5, pady=5)
 
 #escribir contraseñas
-label_contra = ttk.Label(texto, text="Ingrese una usuario:")
+label_contra = ttk.Label(texto, text="Ingrese una pagina:")
 label_contra.pack(anchor='w', padx=5, pady=5)
-usu = ttk.Entry(texto)
-usu.pack(anchor='w',padx=5, pady=5)
+pag = ttk.Entry(texto)
+pag.pack(anchor='w',padx=5, pady=5)
 label_contra = ttk.Label(texto, text="Ingrese su contraseña:")
 label_contra.pack( anchor='w', padx=5, pady=5)
 contra = ttk.Entry(texto)
